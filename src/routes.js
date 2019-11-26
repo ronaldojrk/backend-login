ba40427fdb6a9d6c669713d8a6391a9c9deb808d
Ronaldo jose <ronaldo.jrk@gmail.com>
2019-11-26 05:04:20 -0300

    denovo

--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,8 @@ const FavoritosController =require('./controllers/FavoritosController');
 routes.get('/lista',SessionController.index);
 routes.post('/sessions',SessionController.store);
 routes.post('/login',SessionController.login);
+routes.delete('/removerusuario', SessionController.removerUsuario);
+routes.put('/editarusuario', SessionController.editarUsuario);
 
 routes.get('/listafavo',FavoritosController.index);
 routes.get('/listasuasfavoitas',FavoritosController.favo);
